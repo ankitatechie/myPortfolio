d83522d20415cb69c1ade5af1fd2eefec68a560d
ankita agarwal <ankitaagarwal@sksmotorss-MacBook-Air.local>
2018-09-28 01:39:26 +0530

    git icon added

--- app/js/index.js
+++ app/js/index.js
@@ -89,25 +89,25 @@ $(function() {
   function onScrollInit( items, elemTrigger ) {
     var offset = $(window).height() / 1.6
     items.each( function() {
-      var elem = $(this),
-        animationClass = elem.attr('data-animation'),
-        animationDelay = elem.attr('data-delay');
+      var elem = $(this);
+      var animationClass = elem.attr('data-animation');
+      var animationDelay = elem.attr('data-delay');
 
-        elem.css({
-          '-webkit-animation-delay':  animationDelay,
-          '-moz-animation-delay':     animationDelay,
-          'animation-delay':          animationDelay,
-        });
+      elem.css({
+        '-webkit-animation-delay':  animationDelay,
+        '-moz-animation-delay':     animationDelay,
+        'animation-delay':          animationDelay,
+      });
 
-        var trigger = (elemTrigger) ? trigger : elem;
+      var trigger = (elemTrigger) ? trigger : elem;
 
-        trigger.waypoint(function() {
-          elem.addClass('animated').addClass(animationClass);
-          if (elem.get(0).id === 'gallery') mixClear(); //OPTIONAL
-          },{
-            triggerOnce: true,
-            offset: offset,
-        });
+      trigger.waypoint(function() {
+        elem.addClass('animated').addClass(animationClass);
+        if (elem.get(0).id === 'gallery') mixClear(); //OPTIONAL
+        },{
+          triggerOnce: true,
+          offset: offset,
+      });
     });
   }
 
